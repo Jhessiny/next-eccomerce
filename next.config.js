925e218a40a0cee280d5db771c03252c6503bf3e
jhessiny <jhessinymattos@gmail.com>
2023-01-11 18:09:22 -0300

    feat: add redirect from / to / products

--- next.config.js
+++ next.config.js
@@ -14,6 +14,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/products",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
