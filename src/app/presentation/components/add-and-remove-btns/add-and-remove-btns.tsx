1c8162c2f52276f129caef15377e4bd981bde3e3
jhessiny <jhessinymattos@gmail.com>
2023-01-12 19:08:26 -0300

    feat: adjust styles

--- src/app/presentation/components/add-and-remove-btns/add-and-remove-btns.tsx
+++ src/app/presentation/components/add-and-remove-btns/add-and-remove-btns.tsx
@@ -11,12 +11,12 @@ const btnClasses =
 
 export const AddAndRemoveBtns = ({ addFn, value, removeFn }: Props) => {
   return (
-    <div className="border-2 w-24 flex mt-4 ">
-      <button onClick={addFn} className={btnClasses}>
+    <div className=" w-24 flex mt-4 ">
+      <button onClick={addFn} className={`${btnClasses} rounded-l-sm`}>
         +
       </button>
-      <span className="w-8 py-2">{value}</span>
-      <button onClick={removeFn} className={btnClasses}>
+      <span className="w-8 border-[1px] py-2">{value}</span>
+      <button onClick={removeFn} className={`${btnClasses} rounded-r-sm`}>
         -
       </button>
     </div>
