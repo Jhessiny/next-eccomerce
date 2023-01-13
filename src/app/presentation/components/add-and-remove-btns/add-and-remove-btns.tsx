2402072ce4d7f68ff1ead168eba5bf3291e88e2f
jhessiny <jhessinymattos@gmail.com>
2023-01-13 18:48:51 -0300

    fix: change order

--- src/app/presentation/components/add-and-remove-btns/add-and-remove-btns.tsx
+++ src/app/presentation/components/add-and-remove-btns/add-and-remove-btns.tsx
@@ -12,12 +12,12 @@ const btnClasses =
 export const AddAndRemoveBtns = ({ addFn, value, removeFn }: Props) => {
   return (
     <div className=" w-24 flex mt-4 ">
-      <button onClick={addFn} className={`${btnClasses} rounded-l-sm`}>
-        +
-      </button>
-      <span className="w-8 border-[1px] py-2">{value}</span>
       <button onClick={removeFn} className={`${btnClasses} rounded-r-sm`}>
         -
+      </button>
+      <span className="w-8 border-[1px] py-2">{value}</span>
+      <button onClick={addFn} className={`${btnClasses} rounded-l-sm`}>
+        +
       </button>
     </div>
   );
