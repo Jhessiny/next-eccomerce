c18b1473a92cf1462fa20b0e29d1b04195eeba9b
jhessiny <jhessinymattos@gmail.com>
2023-01-13 18:44:40 -0300

    feat: show cart amount

--- src/app/presentation/components/layout/components/header/components/nav-item/nav-item.tsx
+++ src/app/presentation/components/layout/components/header/components/nav-item/nav-item.tsx
@@ -3,11 +3,12 @@ import React, { PropsWithChildren } from "react";
 
 type Props = PropsWithChildren & {
   href: string;
+  className?: string;
 };
 
-export const NavItem = ({ children, href }: Props) => {
+export const NavItem = ({ children, href, className }: Props) => {
   return (
-    <li className="p-4">
+    <li className={`p-4 ${className}`}>
       <Link href={href}>{children}</Link>
     </li>
   );
--- src/app/presentation/components/layout/components/header/header.tsx
+++ src/app/presentation/components/layout/components/header/header.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { NavItem } from "./components/nav-item/nav-item";
+import { RiShoppingCartLine } from "react-icons/ri";
+import { useCartSelector } from "../../../../hooks";
 
 export const Header = () => {
+  const { cartAmount } = useCartSelector();
   return (
-    <nav className="bg-primary-main text-white mb-10 flex justify-end">
+    <nav className="bg-primary-main text-white mb-10 flex justify-end pr-2">
       <ul className="flex justify-end mr-2">
         <NavItem href="/products">Products</NavItem>
         <NavItem href="/">Profile</NavItem>
-        <NavItem href="/">Cart</NavItem>
+        <NavItem href="/" className="relative flex items-center">
+          <RiShoppingCartLine size={20} />{" "}
+          <span className="bg-orange-400 flex justify-center items-center w-6 h-6  text-sm rounded-full absolute top-1 right-0">
+            {cartAmount}
+          </span>
+        </NavItem>
       </ul>
     </nav>
   );
