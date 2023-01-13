6470a243c61c8ab68f9cef9c1bcf81bddf292432
jhessiny <jhessinymattos@gmail.com>
2023-01-13 18:48:17 -0300

    feat: add and remove item from cart

--- src/app/presentation/components/product-item/product-item.tsx
+++ src/app/presentation/components/product-item/product-item.tsx
@@ -1,14 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useCallback } from "react";
 import { ProductModel } from "../../../domain/models";
 import { formatBRLPrice } from "../../../helpers/format-currency";
 import { Button } from "..";
+import { useCartSelector } from "../../hooks";
+import { stopPropagation } from "../../helpers";
 
 type Props = ProductModel;
 
 export const ProductItem = React.forwardRef<HTMLAnchorElement, Props>(
-  ({ id, title, images, price }, ref) => {
+  ({ id, title, images, price, thumbnail }, ref) => {
+    const { increaseItemAmount, getCartItem, removeCartItem } =
+      useCartSelector();
+
+    const handleAddItem = useCallback(
+      (e: React.MouseEvent<HTMLButtonElement>) => {
+        stopPropagation(e);
+        increaseItemAmount({ id, title, price, thumbnail });
+      },
+      [increaseItemAmount, id]
+    );
+
+    const handleRemoveItem = useCallback(
+      (e: React.MouseEvent<HTMLButtonElement>) => {
+        stopPropagation(e);
+        removeCartItem(id);
+      },
+      [removeCartItem, id]
+    );
+
+    const cartHasItem = Boolean(getCartItem(id));
+
     return (
       <Link
         href={`/products/${id}`}
@@ -25,9 +48,16 @@ export const ProductItem = React.forwardRef<HTMLAnchorElement, Props>(
         />
         <p className="text-2xl font-bold">{title}</p>
         <p>{formatBRLPrice(price)}</p>
-        <Button className="mt-3" action={() => null}>
-          add to cart
-        </Button>
+        {!cartHasItem && (
+          <Button className="mt-3" action={handleAddItem}>
+            add to cart
+          </Button>
+        )}
+        {cartHasItem && (
+          <Button className="mt-3" action={handleRemoveItem}>
+            remove from cart
+          </Button>
+        )}
       </Link>
     );
   }
