569820c10bae354e081bc65e12f3dd57d3fe1dfd
jhessiny <jhessinymattos@gmail.com>
2023-02-04 21:59:21 -0300

    feat: adjust CartItemsList styles to allow it to be used in checkout page

--- src/app/presentation/components/cart/components/cart-items-list/cart-items-list.tsx
+++ src/app/presentation/components/cart/components/cart-items-list/cart-items-list.tsx
@@ -1,21 +1,29 @@
 import { useRouter } from "next/router";
 import React from "react";
+import { twMerge } from "tailwind-merge";
 import { formatUSDPrice } from "../../../../../helpers/format-currency";
 import { useCartSelector } from "../../../../hooks";
 import { Button } from "../../../button/button";
 import { CartItemComponent } from "../cart-item/cart-item";
 
 type Props = {
+  isSideCart?: boolean;
   btn: {
     action: React.MouseEventHandler<HTMLButtonElement>;
     text: string;
+    fullWidth?: boolean;
   };
 };
 
-export const CartItemsList = ({ btn }: Props) => {
+export const CartItemsList = ({
+  btn: { action, text, fullWidth = false },
+  isSideCart = false,
+}: Props) => {
   const { cart: cartItems, getTotalPrice } = useCartSelector();
   const isEmpty = !Boolean(cartItems.length);
   const totalCart = formatUSDPrice(getTotalPrice());
+
+  const btnClasses = fullWidth ? "w-full" : "w-auto";
 
   return (
     <div>
@@ -26,7 +34,11 @@ export const CartItemsList = ({ btn }: Props) => {
             Your items
           </h2>
           {cartItems.map((item) => (
-            <CartItemComponent key={item.id} {...item} />
+            <CartItemComponent
+              key={item.id}
+              {...item}
+              isSideCart={isSideCart}
+            />
           ))}
 
           <div className="flex justify-between mt-4">
@@ -34,9 +46,11 @@ export const CartItemsList = ({ btn }: Props) => {
             <p>{totalCart}</p>
           </div>
 
-          <Button className="w-full mt-6" action={btn.action}>
-            {btn.text}
-          </Button>
+          <div className="flex justify-end">
+            <Button className={twMerge("mt-6", btnClasses)} action={action}>
+              {text}
+            </Button>
+          </div>
         </>
       )}
     </div>
--- src/app/presentation/pages/checkout-page/checkout-page.tsx
+++ src/app/presentation/pages/checkout-page/checkout-page.tsx
@@ -1,14 +1,15 @@
 import { useSession } from "next-auth/react";
 import React from "react";
+import { BackButton } from "../../components";
 import { CartItemsList } from "../../components/cart/components";
 
 export const CheckoutPage = () => {
-  const {} = useSession({ required: true });
+  useSession({ required: true });
+
   return (
-    <div>
-      <div className="overflow-y-scroll h-full">
-        <CartItemsList btn={{ action: () => null, text: "Go to payment" }} />
-      </div>
+    <div className="overflow-y-scroll h-full max-w-256 mx-auto">
+      <BackButton />
+      <CartItemsList btn={{ action: () => null, text: "Go to payment" }} />
     </div>
   );
 };
